0945dc76afb4ec9a140c2f76fa510f977f08dc26
jschang19 <wwwsean13579@gmail.com>
2023-12-17 05:20:09 +0800

    refactor: adjust prompts

--- ChatGPT/src/ChatGPT.cpp
+++ ChatGPT/src/ChatGPT.cpp
@@ -1,9 +1,9 @@
 #include "../include/ChatGPT.h"
+#include "../include/Error.h"
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include <iostream>
 #include <vector> 
-#include "../include/Error.h"
 #include <string>
 
 OpenAI::Message::Message(std::string role, std::string content) : role(role), content(content){}
@@ -23,14 +23,12 @@ OpenAI::ChatCompletion OpenAI::ChatGPT::askChatGPT(const std::string& role) {
         throw std::invalid_argument("Error:there is no prompt message, please use Add_prompts() to add prompt in Chatgpt");
     }
     nlohmann::json j;
-    j["model"] = "gpt-4-1106-preview";
+    j["model"] = "gpt-3.5-turbo-1106";
     j["messages"] = nlohmann::json::parse("[" + prompt_message + "]");
     j["response_format"] = {{"type", "json_object"}};
-    j["temperature"] = 1;
+    j["temperature"] = 0.8;
     j["max_tokens"] = 1000;
     j["n"] = 1;
-
-    std::cout<< j.dump() << std::endl;
 
     auto response = cpr::Post(cpr::Url{m_link}, 
                               cpr::Body{j.dump()}, 
@@ -46,11 +44,13 @@ OpenAI::ChatCompletion OpenAI::ChatGPT::askChatGPT(const std::string& role) {
             throw OpenAI::Error{j_response.dump()};
         }
     } catch (std::exception& e) {
+        std::cout << "\033[1;90mDEV MESSAGE: API 出現錯誤\033[0m\n" << std::endl;
         std::cerr << "Error: " << e.what() << std::endl;
     }
     try {
         j = nlohmann::json::parse(response);
     }catch(std::exception& e){
+        std::cout << "\033[1;90mDEV MESSAGE: API 出現錯誤\033[0m\n" << std::endl;
         std::cerr<<"Error: "+response;
     }
     if(!j.contains("error")) {
@@ -102,11 +102,11 @@ void OpenAI::ChatGPT::Add_prompt(const Message& new_prompt){
 }
 
 std::string OpenAI::ChatGPT::PromptsToStringContent(){
-    std :: string return_string="{\"role\": \"system\", \"content\": \"You are a text game system that generates interesting options to make young college players choose and laugh. you must produce content in Traditional Chinese.\"},";
+    std :: string return_string="{\"role\": \"system\", \"content\": \"You are a text game system that generates options to make young college players choos. you must produce content in Traditional Chinese.\"},";
     for(int i=0; i<this->prompts.size(); i++){
         return_string += " {\"role\": \"" + this->prompts[i].role + "\" , \"content\": \"" + this->prompts[i].content;
         if (i == this->prompts.size()-1){
-            return_string += "，幫我想 4 個好笑且具有創意的遊戲情境讓我選擇，並確保選項都跟之前我的選擇行動有關，都以「 你 」當作開頭，都以句號結尾，請用一個 key 叫 options 的 JSON 物件格式回覆我，而這個 options 的 value 是一個 Array，每個陣列元素要包含 id 跟 text 兩個 key，id 代號為 a, b, c, d";
+            return_string += "，幫我想 4 個搞笑且具有創意的遊戲情境讓我選擇，並確保選項都跟之前我的選擇行動有關，規則：1.都以「你」當作開頭,2.不要加「可以」這個字,3.都以句號結尾，不要用疑問句。都用一個 key 叫 options 的 JSON 物件格式回覆我，而這個 options 的 value 是一個 Array，每個陣列元素要包含 id 跟 text 兩個 key，id 代號為 a, b, c, d";
         }
         return_string += "\"}";
         //處理換行符號
