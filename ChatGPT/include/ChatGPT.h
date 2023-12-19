7a427d25687c4c4fcc1d72420a87a2148339694a
jschang19 <wwwsean13579@gmail.com>
2023-12-19 22:45:50 +0800

    refactor: organize renaming

--- ChatGPT/include/ChatGPT.h
+++ ChatGPT/include/ChatGPT.h
@@ -12,15 +12,14 @@ namespace OpenAI {
     class ChatGPT {
         public:
             explicit ChatGPT(const std::string& token);
-            OpenAI::ChatCompletion askChatGPT(const std::string& role);
-            std::string askWhisper(const std::string& audio_path);
+            OpenAI::ChatCompletion askChatGPT(const std::string& role, bool isEnding);
             std::vector< Message > prompts;
-            void Add_prompt(const Message& prompt);
+            void addPrompt(const Message& prompt);
 
         private:
             std::string m_token;
             std::string m_link;
-
-            std::string PromptsToStringContent();
+            std::string formatPrompt(const Message& prompt, bool isLast, bool isEnding);
+            std::string promptsToStringContent(bool isEnding);
     };
 }
